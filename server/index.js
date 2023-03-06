0b247e53796b42213d91d1e0a237ded161afba5e
vozar.david <vozar.david8@gmail.com>
2023-03-06 10:30:05 +0100

    refactor: remove commented code

--- server/index.js
+++ server/index.js
@@ -7,15 +7,6 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
 
-// const db = require('./src/db/models/index');
-// async () => {
-//   try {
-//     await db.sync();
-//   } catch (e) {
-//     console.log(e);
-//   }
-// };
-
 app.listen(PORT, () => {
   console.log(`Weather App started on: ${PORT}`);
 });
