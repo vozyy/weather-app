55d7607cc3daf391ea9d0fd501bdec0e7f000448
vozar.david <vozar.david8@gmail.com>
2023-03-08 20:20:28 +0100

    feature: add validation on client side

--- client/src/components/RegisterForm/RegisterForm.js
+++ client/src/components/RegisterForm/RegisterForm.js
@@ -8,29 +8,52 @@ export default function RegisterForm() {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
 
+  const [registrationMessage, setRegistrationMessage] = useState('');
+
   const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
-  const [errorMessage, setErrorMessage] = useState('');
+  function validateCredentials() {
+    if (username.length < 4) {
+      setRegistrationMessage('Username has to be at least 4 characters long');
+    } else if (password.length < 8) {
+      setRegistrationMessage('Password must be at least 8 charcaters long');
+    } else if (!password.match(/[!@#$%^&*]/) || !password.match(/[0-9]/)) {
+      setRegistrationMessage(
+        'Password must contain at least one number and one special character'
+      );
+    } else if (password !== confirmPassword) {
+      setRegistrationMessage('Passwords do not match');
+    } else {
+      setRegistrationMessage('');
+    }
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    validateCredentials();
 
     try {
       const data = {
         username: username,
         email: email,
         password: password,
+        confirmPassword: confirmPassword,
       };
 
-      await fetch(`${backendUrl}/api/user`, {
+      const response = await fetch(`${backendUrl}/api/user`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(data),
       });
+      const responseBody = await response.json();
+      if (responseBody.message === 'Email already in use') {
+        setRegistrationMessage(responseBody.message);
+      }
     } catch (error) {
-      setErrorMessage(error.message);
+      console.log(error);
+      setRegistrationMessage('Server Error');
     }
   };
 
@@ -42,7 +65,7 @@ export default function RegisterForm() {
           className='register-input username'
           type='text'
           id='username'
-          // required
+          required
           onChange={(e) => setUsername(e.target.value)}
         />
         <label htmlFor='email'>Email</label>
@@ -51,26 +74,25 @@ export default function RegisterForm() {
           type='email'
           id='email'
           onChange={(e) => setEmail(e.target.value)}
-
-          //  required
+          required
         />
         <label htmlFor='password'>Password</label>
         <input
           className='register-input'
-          type='password'
+          type='text'
           id='password'
           onChange={(e) => setPassword(e.target.value)}
-
-          // required
+          required
         />
         <label htmlFor='password-confirm'>Confirm password</label>
         <input
           className='register-input'
-          type='password'
+          type='text'
           id='password-confirm'
           onChange={(e) => setConfirmPassword(e.target.value)}
-          // required
+          required
         />
+        <p className='registration-error-message'>{registrationMessage}</p>
         <button className='register-button'>Register</button>
         <Link to='#' className='redirect-to-login'>
           Already have an account?
