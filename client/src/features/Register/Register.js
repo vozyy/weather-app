39ac6c523acccb3d711414ee2c488a8c797c241f
vozar.david <vozar.david8@gmail.com>
2023-03-08 10:03:51 +0100

    feat: create component for registration

---
+++ client/src/features/Register/Register.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RegisterForm from '../../components/RegisterForm/RegisterForm';
+import LogoTitle from '../../components/LogoTitle/LogoTitle';
+
+export default function Register() {
+  return (
+    <div>
+      <LogoTitle />
+      <RegisterForm />
+    </div>
+  );
+}
